c58c0c1d380de4d64f7141da3f1406194a8a6c37
Vishruth Muthyala <vishruth.muthyala@hpe.com>
2025-02-27 23:18:50 +0530

    day25

--- day-26-list-comprehension/day-26-nato-alphabet-start/main copy.py
+++ day-26-list-comprehension/day-26-nato-alphabet-start/main copy.py
@@ -6,6 +6,7 @@ with open("nato_phonetic_alphabet.csv") as file:
     data = pd.read_csv(file)
     phonetic_alphabet_dict = {row.letter:row.code for (index, row) in data.iterrows()}
     
+    
 # #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word:").upper()
 output_list = [phonetic_alphabet_dict[letter] for letter in word]
